e66e3185d4666d99752e92022187e935a8af494c
Andre <andrerodrisantos15@gmail.com>
2022-03-28 16:32:34 -0300

    Criação de Componentes e estilos

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import style from "./Botao.module.scss"
+
+
+class Botao extends React.Component{
+    render(){
+return(
+    <button className={style.botao}>
+        PLIM
+    </button>
+)
+    }
+}
+export default Botao
+
+
